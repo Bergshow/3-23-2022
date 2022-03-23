6b6963b06d970cf72e1c7b39c8d6ebaed392dde0
Bergshow <99883224+Bergshow@users.noreply.github.com>
2022-03-23 08:47:56 -0300

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+module.exports = (app) =>{
+    var conexao = require('../config/database') 
+    conexao()
+    var mensagens = require('../models/mensagem')
+    var mygrids = require('../models/mygrid')
+
+    app.get('/', async(req, res) => {
+        var mygrid = await mygrids.find().limit(4).sort({'_id':-1})
+        .then((mygrid) =>{
+            res.render('index.ejs', {dados:mygrid})
+            console.log(mygrid)
+        })
+        .catch(()=>{
+           // res.render('index.ejs')
+        })
+    })
+
+    app.post('/', (req, res) =>{
+  
+
+    var documento = new mensagens ({
+        nome: req.body.first_name,
+        sobrenome: req.body.last_name,
+        email:req.body.email,
+        mensagem: req.body.message
+    })
+    .save()
+    .then(() =>{
+        res.redirect('/')
+    })
+    .catch(() =>{
+        res.send("Não foi possível gravar o documento no Banco de Dados")
+    })
+    })
+}
---
+++ routes/mygrid.js
@@ -0,0 +1,37 @@
+module.exports = (app)=>{
+
+    var conexao = require ('../config/database')
+    //importar o modelo mygrid
+    var modelo = require('../models/mygrid')
+
+    //abrir o formulário mygrid.js
+    app.get('/mygrid', (req,res)=>{
+        //conectar com o database
+        conexao()
+        //buscar todos os documentos da coleção mygrid
+        modelo.find()
+        .then((modelo)=>{
+            res.render('mygrid.ejs', {dados:modelo})
+            .catch(()=>{
+                res.render('mygrid.ejs')
+            })
+        })
+    })
+
+
+    app.post('/mygrid', (req,res)=>{
+        //conectar com o database
+        conexao()
+        //gravar o documento na coleção mygrid
+        var documento = new modelo({
+            titulo:req.body.titulo,
+            texto:req.body.texto
+        }).save()
+        .then(()=>{
+            res.redirect('/mygrid')
+        })
+        .catch(()=>{
+            res.send('Não foi possível gravar os dados no DB')
+        })
+    })
+}
